0e16cc240444e5537f834944bd82d9605fef84ba
Joshua Kousal <josh.kousal@gmail.com>
2023-10-11 10:24:21 +0200

    small changes

---
+++ josh/python.py
@@ -0,0 +1,5 @@
+from talon import Module
+
+mod = Module()
+# this declares a tag in the user namespace (i.e. 'user.python')
+mod.tag("python", desc="basic commands for working with python within a window are available")
